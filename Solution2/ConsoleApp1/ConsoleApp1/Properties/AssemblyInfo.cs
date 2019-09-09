56951fbf32723b7934ea6955df99750ab3dabdca
Sss41 <silvana.souza63@gmail.com>
2019-09-09 15:55:38 -0300

    sdasad

--- Solution2/ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
+++ Solution2/ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5386ac94-0319-4ed8-a78a-65fb46b1fca7")]
+[assembly: Guid("a255493e-6a27-4356-a03d-26db60091a3c")]
 
 // Version information for an assembly consists of the following four values:
 //
