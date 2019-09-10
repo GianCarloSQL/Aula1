759691c08a34e3bc49a569d94f5b634a43b873e1
Gian Carlo Giovanella <gcgiovanella@furb.br>
2019-09-10 17:24:48 -0300

    livro

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+           Console.WriteLine("Informe o livro a ser consultado: ");
+           var numeroLivro = Console.ReadLine();
+            if (numeroLivro == "123456")
+            {
+                Console.WriteLine("Livro indisponivel");
+            }
+            else {
+                Console.WriteLine("Deseja alocar o livro? (1-sim, 2-nao)");
+                var resposta = Console.ReadLine();
+                if (resposta == "1")
+                {
+                    Console.WriteLine("livro Alocado");
+                }
+
+            }
+        }
+
+    }
+}
