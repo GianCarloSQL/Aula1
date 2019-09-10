935a52c4276d416b97d134d572f72c4a00295a7b
Gian Carlo Giovanella <gcgiovanella@furb.br>
2019-09-10 15:41:51 -0300

    tes

--- Solution1/Solution1/ConsoleApp1/Program.cs
+++ Solution1/Solution1/ConsoleApp1/Program.cs
@@ -5,12 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1{
-    class Program{
+    class Program<T> {
         public static void Main(string[] args) {
           
-            Console.WriteLine("Ola Mundo");
+            Console.WriteLine("");
+            Console.WriteLine(Int32.MaxValue);
+            int x = 2147483647;
+            int y = 1;
+
             Console.ReadKey();
-            // teste
+            
 
         }
     }
