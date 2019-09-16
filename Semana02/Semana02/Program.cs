cb8b33277bcf89501d04f06c6a8f16dacf4a3bee
Gian Carlo Giovanella <gcgiovanella@furb.br>
2019-09-16 08:43:58 -0300

    semana02

---
+++ Semana02/Semana02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
