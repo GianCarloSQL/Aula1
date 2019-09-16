c33baa2175c1a0848df23bca60e4a60248ed9d2c
Gian Carlo Giovanella <gcgiovanella@furb.br>
2019-09-16 15:39:43 -0300

    split string

--- Semana02/Semana02/Program.cs
+++ Semana02/Semana02/Program.cs
@@ -11,8 +11,41 @@ namespace Semana02
     {
         static void Main(string[] args)
         {
-            BuscaAutomovel("nome:Jetta,marca:Volkswagen,ano:2014,cambio:automatico,descricao:Porta direita nao abre;nome:Gol,marca:Volkswagen,ano:2015,cambio:manual,descricao:Faz barulho estranho;nome:Corolla,marca:Toyota,ano:2019,cambio:automatico,descricao:Carro de tiozao;nome:Mustang,marca:Ford,ano:2010,cambio:automatico,descricao:Voce nao pode pagar");
+            /*
+            string[] aux = BuscaAutomovel("nome:Jetta,marca:Volkswagen,ano:2014,cambio:automatico,descricao:Porta direita nao abre;nome:Gol,marca:Volkswagen,ano:2015,cambio:manual,descricao:Faz barulho estranho;nome:Corolla,marca:Toyota,ano:2019,cambio:automatico,descricao:Carro de tiozao;nome:Mustang,marca:Ford,ano:2010,cambio:automatico,descricao:Voce nao pode pagar", Console.ReadLine());
+            for (int i = 0; i < aux.Length; i++)
+            {
+                Console.WriteLine(aux[i]);
+            }
+            */
+
+            //    BuscaAutomovel("nome:Jetta,marca:Volkswagen,ano:2014,cambio:automatico,descricao:Porta direita nao abre;nome:Gol,marca:Volkswagen,ano:2015,cambio:manual,descricao:Faz barulho estranho;nome:Corolla,marca:Toyota,ano:2019,cambio:automatico,descricao:Carro de tiozao;nome:Mustang,marca:Ford,ano:2010,cambio:automatico,descricao:Voce nao pode pagar");
+
+            Console.WriteLine(" As pessoas que contem mais de 18 anos são:" + procuraPessoa("nome: Felipe, idade: 27; nome: Giomar,idade: 17; nome: Edson,Idade: 19; nome: Ericledson,idade: 75; nome: Junior,idade: 45"));
+            Thread.Sleep(10000);
         }
+
+        private static string procuraPessoa(string BandoDeDados)
+        {
+            string retorno = "";
+            var aux = BandoDeDados.Split(';');
+            foreach (var item in aux)
+            {
+                var split = item.Split(',');
+                for (int i = 0; i < 2; i++)
+                {
+                    split[i] = split[i].Split(':')[1];
+                }
+                if (int.Parse(split[1]) >= 18)
+                {
+                    retorno += "\n" + split[0];
+                }
+            }
+            return retorno;
+        }
+
+
+
 
         private static void BuscaAutomovel(string bandoDeDados)
         {
@@ -37,11 +70,34 @@ namespace Semana02
                     //split[4] = descricao
                     if (split[0] == veiculoBusca)
                     {
-                        Console.WriteLine($"Nome Veiculo: {split[0]}\nMarca: {split[1]}\nAno: {split[2]}\nCambio: {split[3]}\nDescricao: {split[4]}");
+                        Console.WriteLine(string.Format("Nome Veiculo: {0}\nMarca: {1}\nAno: {2}\nCambio: {3}\nDescricao: {4}", split));
+                        // Console.WriteLine($"Nome Veiculo: {split[0]}\nMarca: {split[1]}\nAno: {split[2]}\nCambio: {split[3]}\nDescricao: {split[4]}");
                     }
                 }
             }
         }
+
+        private static string[] BuscaAutomovel(string bandoDeDados, string veiculoBusca)
+        {
+            var aux = bandoDeDados.Split(';');
+
+            foreach (var item in aux)
+            {
+                var split = item.Split(',');
+                for (int i = 0; i < 5; i++)
+                {
+                    split[i] = split[i].Split(':')[1];
+                }
+
+                if (split[0] == veiculoBusca)
+                {
+                    return split;
+                }
+
+            }
+            return null;
+        }
+
 
 
         public static string ForSplit()
